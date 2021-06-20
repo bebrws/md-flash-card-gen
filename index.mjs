b9d649d58834587be1535c901e90c7a503459604
Brad Barrows <brad@manscaped.com>
2021-06-20 11:48:00 -0700

    Create flash cards from the cli!

---
+++ index.mjs
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+import rlsync from "readline-sync";
+import chalk from "chalk";
+import fs from "fs";
+
+console.log(
+  chalk.red.bold(
+    "\n\nAll output from this session is written and appended to the local file flash-card-data.md\n\n"
+  )
+);
+
+while (true) {
+  let fcfront = rlsync.question(
+    chalk.green.bold(
+      'Question or flash card "front" - empty string will exit the program:'
+    )
+  );
+
+  if (!fcfront) process.exit(0);
+
+  let fcanswers = [];
+  let fcanswer = "";
+  do {
+    fcanswer = rlsync.question(
+      chalk.yellow.bold(
+        "Answer item for back side of flash card? - emptry string will mean done"
+      )
+    );
+    if (fcanswer) fcanswers.push(fcanswer);
+  } while (fcanswer);
+
+  console.log("fcanswers", fcanswers);
+  const fcitems = fcanswers.reduce((acc, cur) => {
+    return (acc += `<li>${cur}</li>\n        `);
+  }, "");
+
+  console.log("fcitems", fcitems);
+
+  const output = `
+  ### ${fcfront}
+
+  <details>
+    <summary>Click here for the solution</summary>
+      <ul>
+        ${fcitems}
+      </ul>
+  </details>
+  `;
+
+  await fs.writeFileSync("flash-card-data.md", output, { flag: "a+" });
+}
